211c9e7a9f6f4d7e47d20d1ec13071a44d81c194
ServerConnection <76793112+ServerConnection@users.noreply.github.com>
2021-02-17 01:10:59 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import re
+import ast
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+
+with open('getproxy/__init__.py', 'rb') as f:
+    version = str(ast.literal_eval(_version_re.search(
+        f.read().decode('utf-8')).group(1)))
+
+
+requirements = [
+    'click==6.0',
+    'requests==2.13.0',
+    'gevent==1.2.1',
+    'geoip2==2.5.0',
+    'maxminddb==1.5.2',
+    'retrying==1.3.3'
+]
+
+setup(
+    name='getproxy',
+    version=version,
+    description="get proxy",
+    long_description=readme,
+    author="fate0",
+    author_email='fate0@fatezero.org',
+    url='https://github.com/fate0/getproxy',
+    packages=find_packages(),
+    package_dir={},
+    entry_points={
+        'console_scripts': [
+            'getproxy=getproxy.cli:main'
+        ]
+    },
+    include_package_data=True,
+    install_requires=requirements,
+    license="BSD license",
+    zip_safe=False,
+    keywords='getproxy',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
